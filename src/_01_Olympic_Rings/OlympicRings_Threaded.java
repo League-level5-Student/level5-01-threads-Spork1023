4f9278256b3aebb2febf66f5dd6d827cd8aad679
Ryder Casazza <rydercasazza@me.com>
2019-12-19 22:08:31 -0800

    finished olympic rings threaded and started advanced robot race

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,75 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot r1 = new Robot(300, 700);
+		Robot r2 = new Robot(500, 800);
+		Robot r3 = new Robot(700, 700);
+		Robot r4 = new Robot(900, 800);
+		Robot r5 = new Robot(1100, 700);
 
+		r1.setSpeed(400);
+		r2.setSpeed(400);
+		r3.setSpeed(400);
+		r4.setSpeed(400);
+		r5.setSpeed(400);
+		
+		r1.penDown();
+		r1.setPenWidth(10);
+		r2.penDown();
+		r2.setPenWidth(10);
+		r3.penDown();
+		r3.setPenWidth(10);
+		r4.penDown();
+		r4.setPenWidth(10);
+		r5.penDown();
+		r5.setPenWidth(10);
+
+		Thread t1 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r1.turn(1);
+				r1.move(3);
+			}
+		});
+
+		Thread t2 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r2.turn(1);
+				r2.move(3);
+			}
+		});
+
+		Thread t3 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r3.turn(1);
+				r3.move(3);
+			}
+		});
+
+		Thread t4 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r4.turn(1);
+				r4.move(3);
+			}
+		});
+
+		Thread t5 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r5.turn(1);
+				r5.move(3);
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+	}
 }
-
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,58 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+	// Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
+
+	// 1. make a main method
+	public static void main(String[] args) {
+		Random r = new Random();
+		int y = 999;
+		// 2. create an array of 5 robots.
+		Robot[] robots = new Robot[5];
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot();
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+		robots[0].moveTo(200, 800);
+		robots[1].moveTo(400, 800);
+		robots[2].moveTo(600, 800);
+		robots[3].moveTo(800, 800);
+		robots[4].moveTo(1000, 800);
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		Thread t1 = new Thread(() -> {
+			robots[0].move(r.nextInt(50));
+		});
+
+		Thread t2 = new Thread(() -> {
+			robots[1].move(r.nextInt(50));
+		});
+		Thread t3 = new Thread(() -> {
+			robots[2].move(r.nextInt(50));
+		});
+		Thread t4 = new Thread(() -> {
+			robots[3].move(r.nextInt(50));
+		});
+		Thread t5 = new Thread(() -> {
+			robots[4].move(r.nextInt(50));
+		});
+		while (y != 0) {
+			t1.start();
+			t2.start();
+			t3.start();
+			t4.start();
+			t5.start();
+			for (int i = 0; i < robots.length; i++) {
+				if (robots[i].getY() < y) {
+					y = robots[i].getY();
+				}
+			}
+		}
+	}
 }
